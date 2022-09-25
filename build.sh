a5f1eef6ce36bd582920bc0a5e8b93ce079f2016
Arvind-4 <lordarvindthegamer@gmail.com>
2022-09-25 14:05:01 +0530

    Updated: manage.py file

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ python3.9 -m pip install -r requirements.txt
 
 echo "Migrating database..."
 
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3.9 web/manage.py makemigrations --noinput
+python3.9 web/manage.py migrate --noinput
 
 echo "Creating superuser..."
 
@@ -15,11 +15,11 @@ DJANGO_SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL}
 DJANGO_SUPERUSER_USERNAME=${DJANGO_SUPERUSER_USERNAME}
 DJANGO_SUPERUSER_PASSWORD=${DJANGO_SUPERUSER_PASSWORD}
 
-python3.9 manage.py createsuperuser \
+python3.9 web/manage.py createsuperuser \
     --email $DJANGO_SUPERUSER_EMAIL \
     --username $DJANGO_SUPERUSER_USERNAME \
     --noinput || true
 
 echo "Collecting static files..."
 
-python3.9 manage.py collectstatic --noinput
+python3.9 web/manage.py collectstatic --noinput
