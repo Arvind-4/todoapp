ec45944e0ea7f4ce886d4fe99a450b5701e61245
Arvind <pingarvindforquestions@gmail.com>
2024-07-22 14:20:14 +0000

    fix: files for deployment

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
-from web.backend.wsgi import application
-app = application
+import os
+from utils import read_env
+
+read_env()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+
+import django
+
+django.setup()
+
+from django.core.wsgi import get_wsgi_application
+
+app = get_wsgi_application()
